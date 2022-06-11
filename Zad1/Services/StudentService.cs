7d1c270f2ba4f9b3b2198f753d19419147518ca4
s19563 <l0372@pjwstk.edu.pl>
2022-06-11 17:25:01 +0200

    Initial commit

---
+++ Zad1/Services/StudentService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Zad1.Models;
+
+namespace Zad1.Services
+{
+    public class StudentService
+    {
+
+        private StudentFileWriter _writer;
+
+        public StudentService()
+        {
+            _writer = new StudentFileWriter();
+        }
+
+        public IEnumerable<Student> getStudents()
+        {
+            List<string> lines = (List<string>)_writer.getStudentsFromFile();
+            List<Student> students = new List<Student>();
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(",");
+            }
+            return students;
+        }
+
+        public string addStudent(Student student)
+        {
+            return "";
+        }
+    }
+}
